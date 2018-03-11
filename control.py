d671be87e036eb721fc9158fe6e0f34ff32ddded
Brent Vollebregt <volleys2468@gmail.com>
2018-03-11 17:48:14 +1300

    Basic structure

---
+++ control.py
@@ -0,0 +1,98 @@
+import string
+from pynput.keyboard import Key
+from pynput.keyboard import Controller as KeyboardController
+from pynput.mouse import Button
+from pynput.mouse import Controller as MouseController
+
+keyboard_controller = KeyboardController()
+mouse_controller = MouseController()
+
+character_keys = [char for char in string.digits] + [char for char in string.ascii_letters] + [char for char in string.punctuation]
+action_keys = {
+    "backspace" : Key.backspace,
+    "caps_lock" : Key.caps_lock,
+    "cmd" : Key.cmd,
+    "ctrl" : Key.ctrl,
+    "delete" : Key.delete,
+    "down" : Key.down,
+    "end" : Key.end,
+    "enter" : Key.enter,
+    "esc" : Key.esc,
+    "f1" : Key.f1,
+    "f2" : Key.f2,
+    "home" : Key.home,
+    "insert" : Key.insert,
+    "left" : Key.left,
+    "menu" : Key.menu,
+    "num_lock" : Key.num_lock,
+    "page_up" : Key.page_up,
+    "pause" : Key.pause,
+    "print_screen" : Key.print_screen,
+    "right" : Key.right,
+    "scroll_lock" : Key.scroll_lock,
+    "shift" : Key.shift,
+    "space" : Key.space,
+    "tab" : Key.tab,
+    "up" : Key.up
+}
+mouse_buttons = {
+    'left' : Button.left,
+    'right' : Button.right,
+    'middle' : Button.middle
+}
+
+def keyboard_type(key):
+    if key in character_keys:
+        keyboard_controller.type(key)
+    elif key in action_keys:
+        keyboard_controller.press( action_keys[key] )
+        keyboard_controller.release( action_keys[key] )
+    else:
+        return False
+    return True
+
+def keyboard_press(key):
+    if key in character_keys:
+        keyboard_controller.press(key)
+    elif key in action_keys:
+        keyboard_controller.press( action_keys[key] )
+    else:
+        return False
+    return True
+
+def keyboard_release(key):
+    if key in character_keys:
+        keyboard_controller.release(key)
+    elif key in action_keys:
+        keyboard_controller.release( action_keys[key] )
+    else:
+        return False
+    return True
+
+def mouse_position(position_vector=None):
+    if position_vector is None:
+        return mouse_controller.position
+    else:
+        mouse_controller.position = position_vector
+        return True
+
+def mouse_move(x, y):
+    mouse_controller.move(x, y)
+    return True
+
+def mouse_click(button, amount):
+    mouse_controller.click(mouse_buttons[button], amount)
+    return True
+
+def mouse_press(button):
+    mouse_controller.press(mouse_buttons[button])
+    return True
+
+def mouse_release(button):
+    mouse_controller.release(mouse_buttons[button])
+    return True
+
+def mouse_scroll(dx, dy):
+    # Can't get side scroll to work and dy can be very unpredictable. So much for "The units of scrolling is undefined" from pynput
+    mouse_controller.scroll(dx, dy)
+    return True
---
+++ server.py
@@ -0,0 +1,115 @@
+from flask import Flask, jsonify, request
+import platform
+import argparse
+import socket
+import string
+import random
+
+import control
+
+# Arguments
+parser = argparse.ArgumentParser(description='A simple server that allows you to control the mouse and keyboard.')
+parser.add_argument('-key', action="store", type=str, default='')
+parser.add_argument('-ip', action="store", type=str, default=socket.gethostbyname(socket.gethostname()))
+parser.add_argument('-port', action="store", type=int, default=8080)
+args = parser.parse_args()
+
+app = Flask(__name__)
+token = ''.join([random.choice(string.printable) for i in range(64)])
+
+def check_token(provided_token):
+    return provided_token == token
+
+@app.route("/info", methods=['GET'])
+def info_route():
+    return jsonify({
+        "os" : platform.system(),
+        "release" : platform.release(),
+        "user": platform.node(),
+        "security" : args.key != ''
+    })
+
+@app.route("/get_token", methods=['POST'])
+def get_token_route():
+    if args.key == '':
+        return jsonify({'success': True, 'token': token})
+    else:
+        if request.json['key'] == args.key:
+            return jsonify({'success': True, 'token': token})
+        else:
+            return jsonify({'success': False, 'token': token})
+
+@app.route("/check_token", methods=['POST'])
+def check_token_route():
+    if not check_token(request.json['token']):
+        return jsonify({'success' : False, 'reason' : 'Invalid Token'})
+
+    return jsonify({'success' : True})
+
+@app.route("/keyboard/type", methods=['POST'])
+def keyboard_type_route():
+    if not check_token(request.json['token']):
+        return jsonify({'success' : False, 'reason' : 'Invalid Token'})
+
+    action = control.keyboard_type(request.json['key'])
+    return jsonify({'success' : action})
+
+@app.route("/keyboard/press", methods=['POST'])
+def keyboard_press_route():
+    if not check_token(request.json['token']):
+        return jsonify({'success' : False, 'reason' : 'Invalid Token'})
+
+    action = control.keyboard_press(request.json['key'])
+    return jsonify({'success': action})
+
+@app.route("/keyboard/release", methods=['POST'])
+def keyboard_release_route():
+    if not check_token(request.json['token']):
+        return jsonify({'success' : False, 'reason' : 'Invalid Token'})
+
+    action = control.keyboard_release(request.json['key'])
+    return jsonify({'success': action})
+
+# TODO Mouse Position
+
+@app.route("/mouse/move", methods=['POST'])
+def mouse_move_route():
+    if not check_token(request.json['token']):
+        return jsonify({'success' : False, 'reason' : 'Invalid Token'})
+
+    action = control.mouse_move(request.json['x'], request.json['y'])
+    return jsonify({'success': action})
+
+@app.route("/mouse/click", methods=['POST'])
+def mouse_click_route():
+    if not check_token(request.json['token']):
+        return jsonify({'success' : False, 'reason' : 'Invalid Token'})
+
+    action = control.mouse_click(request.json['button'], request.json['amount'])
+    return jsonify({'success': action})
+
+@app.route("/mouse/press", methods=['POST'])
+def mouse_press_route():
+    if not check_token(request.json['token']):
+        return jsonify({'success' : False, 'reason' : 'Invalid Token'})
+
+    action = control.mouse_press(request.json['button'])
+    return jsonify({'success': action})
+
+@app.route("/mouse/release", methods=['POST'])
+def mouse_release_route():
+    if not check_token(request.json['token']):
+        return jsonify({'success' : False, 'reason' : 'Invalid Token'})
+
+    action = control.mouse_release(request.json['button'])
+    return jsonify({'success': action})
+
+@app.route("/mouse/scroll", methods=['POST'])
+def mouse_scroll_route():
+    if not check_token(request.json['token']):
+        return jsonify({'success' : False, 'reason' : 'Invalid Token'})
+
+    action = control.mouse_scroll(request.json['dx'], request.json['dy'])
+    return jsonify({'success': action})
+
+app.run(args.ip, args.port)
